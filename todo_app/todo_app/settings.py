cb58aa377f1210d6c424f710c28a788f95bcfb3e
ipung07 <116989025+ipung07@users.noreply.github.com>
2022-11-17 16:54:21 +0700

    Update settings.py

--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m$!my*c%h+dhv67cn_ixzg4u+z@orwzt(6h@+^w@6v)tk+%9-&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
